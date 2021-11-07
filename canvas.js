3271c3a642cc88b7fc7eae3a11d2750439000af5
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-07 15:19:01 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,6 @@
 console.log("help");
+//First, create a variable called canvas, and select the html element.
+//use document selector, and then a querySelector(), which selects the first element of the stated kind. or, getElementById()
+var canvas = document.querySelector('canvas');
+//now to alter the width of the canvas, select canvas.width, and set it to the window.innerWidth(self explanatory)
+canvas.width = window.innerWidth; //makes canvas expand to full width, excluding the margin
